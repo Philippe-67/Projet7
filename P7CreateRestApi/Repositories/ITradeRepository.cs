d454a57b3b390ad7c81a0a929f4a5856da6861fa
Philippe CHARLES <philippe.charles@axa.fr>
2023-10-01 19:43:58 +0200

    complément Trade

--- P7CreateRestApi/Repositories/ITradeRepository.cs
+++ P7CreateRestApi/Repositories/ITradeRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace P7CreateRestApi.Repositories
+﻿using P7CreateRestApi.Domain;
+
+namespace P7CreateRestApi.Repositories
+
+
 {
     public interface ITradeRepository
     {
+        Task<IEnumerable<Trade>> GetAllAsync();
+        Task<Trade> GetByIdAsync(int id);
+        Task AddAsync(Trade trade);
+        Task UpdateAsync(Trade trade);
+        Task DeleteAsync(int id);
     }
 }
+
+
+
--- P7CreateRestApi/Repositories/TradeRepository.cs
+++ P7CreateRestApi/Repositories/TradeRepository.cs
@@ -1,6 +1,50 @@
-﻿namespace P7CreateRestApi.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using P7CreateRestApi.Data;
+using P7CreateRestApi.Domain;
+
+namespace P7CreateRestApi.Repositories
 {
-    public class TradeRepository
+    public class TradeRepository : ITradeRepository
     {
+        private readonly LocalDbContext _context;
+
+        public TradeRepository(LocalDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Trade>> GetAllAsync()
+        {
+            return await _context.Trades.ToListAsync();
+        }
+
+        public async Task<Trade> GetByIdAsync(int id)
+        {
+            return await _context.Trades.FindAsync(id);
+        }
+
+        public async Task AddAsync(Trade trade)
+        {
+            _context.Trades.Add(trade);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Trade trade)
+        {
+            _context.Trades.Update(trade);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var trade = await _context.Trades.FindAsync(id);
+            if (trade != null)
+            {
+                _context.Trades.Remove(trade);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
+
+
